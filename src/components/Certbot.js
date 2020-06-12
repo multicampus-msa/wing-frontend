d41a0cf66d73440d2615634ddce1109484d469ae
StolenMoments <unknown9732@naver.com>
2020-06-12 14:59:18 +0900

    certbot test

--- src/components/Certbot.js
+++ src/components/Certbot.js
@@ -1,2 +1,10 @@
-export default "InyFgBnuIiknGQ68NHS2Q1Tnvva2KAvD7yfQU6JUzFY.PCE0tiBdA9cmwHsDNVdpFOROtcfze_\n" +
-"mVHVNApdGmAFM";
+import React from 'react'
+
+const Certbot = () => {
+    return (
+        "InyFgBnuIiknGQ68NHS2Q1Tnvva2KAvD7yfQU6JUzFY.PCE0tiBdA9cmwHsDNVdpFOROtcfze_\n" +
+        "mVHVNApdGmAFM"
+    )
+}
+
+export default Certbot;
