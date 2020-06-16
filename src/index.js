b726ee60a78f0dd610411a593cc850e95aee4720
StolenMoments <unknown9732@naver.com>
2020-06-16 11:23:51 +0900

    외않되?

--- src/index.js
+++ src/index.js
@@ -4,12 +4,11 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.css'
 import { BrowserRouter } from 'react-router-dom';
-import Playlist from "react-mp3-player";
 
 
 
 ReactDOM.render(<BrowserRouter><App/></BrowserRouter>, document.getElementById('root'));
-//ReactDOM.render(<Playlist tracks={tracks}/>, document.getElementById('player'));
+ReactDOM.render(<div> </div>, document.getElementById('player'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
