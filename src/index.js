4118741613cc2aeb50c558c42d46b000c27323e0
StolenMoments <unknown9732@naver.com>
2020-08-04 22:53:26 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,8 +5,6 @@ import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.css'
 import { BrowserRouter } from 'react-router-dom';
 
-
-
 ReactDOM.render(<BrowserRouter><App/></BrowserRouter>, document.getElementById('root'));
 ReactDOM.render(<div> </div>, document.getElementById('player'));
 
